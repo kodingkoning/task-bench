8de4d5c7809d159a026e0405d4289a2fea313060
yilerat19 <mylin2@illinois.edu>
2021-05-12 00:16:52 -0500

    Update mpi code and one of mpi script

--- experiments/uiuc_cc/metg_compute/mpi_dep_case0.sh
+++ experiments/uiuc_cc/metg_compute/mpi_dep_case0.sh
@@ -12,7 +12,7 @@ echo Variant = $VARIANT
 cores=$(( $(echo $SLURM_JOB_CPUS_PER_NODE | cut -d'(' -f 1) / 2 ))
 
 function sweep {
-    for s in 3 4 5 6 7 8 9; do
+    for s in 5 6 7 8 9 10 11 12 13 14 15 16 17 18; do
         for rep in 0 1 2 3 4; do
             if [[ $rep -le $s ]]; then
 		srun -n $(( $2 * cores )) -N $2 --ntasks-per-node=$cores --cpus-per-task=2 --cpu_bind cores ../../../mpi/bulk_synchronous -kernel compute_bound -iter $(( 1 << (26-s) )) -type $4 -radix ${RADIX:-5} -steps ${STEPS:-1000} -width $(( $2 * cores )) -output-case 0 -output 64
